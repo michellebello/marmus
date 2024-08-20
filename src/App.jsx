4a778c39b70ea6693d2f8dc25649f8451c58c929
michellebello <michellebelloveramendi@gmail.com>
2024-08-20 18:00:57 -0400

    update with hidden password with clicklable eye icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import "./styles/index.css";
-import Login from "./components/Login.jsx";
+import Signup from "./components/Signup.jsx";
 
 function App() {
   axios.get("http://localhost:5443").then((res) => console.log(res.data));
   return (
     <div>
-      <Login />
+      <Signup />
     </div>
   );
 }
